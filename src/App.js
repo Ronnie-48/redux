2976dc08ba2ee4266ae4b74f6d7bcc887fa5e2f4
Ron <Ronnielangat48@gmail.com>
2024-05-15 21:35:44 +0300

    added products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductDetail from './containers/ProductDetail';
+import ProductListing from './containers/ProductListing';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      
+      <Router>
+        <Header />
+      <Routes>
+        <Route index element={<ProductListing />} />
+        <Route path="/product/:productId" element={ProductDetail} />
+        <Route>404 not  found</Route>
+      </Routes>
+
+      </Router>
     </div>
   );
 }
